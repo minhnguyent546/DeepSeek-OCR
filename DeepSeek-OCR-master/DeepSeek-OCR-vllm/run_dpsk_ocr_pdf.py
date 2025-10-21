fca490e93bd58aa3975677790817ef5aeb0f5732
minhnguyent546 <minhnguyent546@gmail.com>
2025-10-21 16:35:44 +0000

    feat: support input path as a directory containing pdf files

--- DeepSeek-OCR-master/DeepSeek-OCR-vllm/run_dpsk_ocr_pdf.py
+++ DeepSeek-OCR-master/DeepSeek-OCR-vllm/run_dpsk_ocr_pdf.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import fitz
 import img2pdf
@@ -6,7 +7,7 @@ import re
 from tqdm import tqdm
 import torch
 from concurrent.futures import ThreadPoolExecutor
- 
+
 
 if torch.version.cuda == '11.8':
     os.environ["TRITON_PTXAS_PATH"] = "/usr/local/cuda-11.8/bin/ptxas"
@@ -14,7 +15,7 @@ os.environ['VLLM_USE_V1'] = '0'
 os.environ["CUDA_VISIBLE_DEVICES"] = '0'
 
 
-from config import MODEL_PATH, INPUT_PATH, OUTPUT_PATH, PROMPT, SKIP_REPEAT, MAX_CONCURRENCY, NUM_WORKERS, CROP_MODE
+from config import MODEL_PATH, PROMPT, SKIP_REPEAT, MAX_CONCURRENCY, NUM_WORKERS, CROP_MODE
 
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
@@ -34,7 +35,7 @@ llm = LLM(
     hf_overrides={"architectures": ["DeepseekOCRForCausalLM"]},
     block_size=256,
     enforce_eager=False,
-    trust_remote_code=True, 
+    trust_remote_code=True,
     max_model_len=8192,
     swap_space=0,
     max_num_seqs=MAX_CONCURRENCY,
@@ -59,19 +60,19 @@ class Colors:
     GREEN = '\033[32m'
     YELLOW = '\033[33m'
     BLUE = '\033[34m'
-    RESET = '\033[0m' 
+    RESET = '\033[0m'
 
 def pdf_to_images_high_quality(pdf_path, dpi=144, image_format="PNG"):
     """
     pdf2images
     """
     images = []
-    
+
     pdf_document = fitz.open(pdf_path)
-    
+
     zoom = dpi / 72.0
     matrix = fitz.Matrix(zoom, zoom)
-    
+
     for page_num in range(pdf_document.page_count):
         page = pdf_document[page_num]
 
@@ -88,9 +89,9 @@ def pdf_to_images_high_quality(pdf_path, dpi=144, image_format="PNG"):
                 background = Image.new('RGB', img.size, (255, 255, 255))
                 background.paste(img, mask=img.split()[-1] if img.mode == 'RGBA' else None)
                 img = background
-        
+
         images.append(img)
-    
+
     pdf_document.close()
     return images
 
@@ -98,18 +99,18 @@ def pil_to_pdf_img2pdf(pil_images, output_path):
 
     if not pil_images:
         return
-    
+
     image_bytes_list = []
-    
+
     for img in pil_images:
         if img.mode != 'RGB':
             img = img.convert('RGB')
-        
+
         img_buffer = io.BytesIO()
         img.save(img_buffer, format='JPEG', quality=95)
         img_bytes = img_buffer.getvalue()
         image_bytes_list.append(img_bytes)
-    
+
     try:
         pdf_bytes = img2pdf.convert(image_bytes_list)
         with open(output_path, "wb") as f:
@@ -148,7 +149,7 @@ def extract_coordinates_and_label(ref_text, image_width, image_height):
     return (label_type, cor_list)
 
 
-def draw_bounding_boxes(image, refs, jdx):
+def draw_bounding_boxes(image, refs, jdx, output_path):
 
     image_width, image_height = image.size
     img_draw = image.copy()
@@ -156,18 +157,18 @@ def draw_bounding_boxes(image, refs, jdx):
 
     overlay = Image.new('RGBA', img_draw.size, (0, 0, 0, 0))
     draw2 = ImageDraw.Draw(overlay)
-    
+
     #     except IOError:
     font = ImageFont.load_default()
 
     img_idx = 0
-    
+
     for i, ref in enumerate(refs):
         try:
             result = extract_coordinates_and_label(ref, image_width, image_height)
             if result:
                 label_type, points_list = result
-                
+
                 color = (np.random.randint(0, 200), np.random.randint(0, 200), np.random.randint(0, 255))
 
                 color_a = color + (20, )
@@ -183,12 +184,12 @@ def draw_bounding_boxes(image, refs, jdx):
                     if label_type == 'image':
                         try:
                             cropped = image.crop((x1, y1, x2, y2))
-                            cropped.save(f"{OUTPUT_PATH}/images/{jdx}_{img_idx}.jpg")
+                            cropped.save(f"{output_path}/images/{jdx}_{img_idx}.jpg")
                         except Exception as e:
                             print(e)
                             pass
                         img_idx += 1
-                        
+
                     try:
                         if label_type == 'title':
                             draw.rectangle([x1, y1, x2, y2], outline=color, width=4)
@@ -199,13 +200,13 @@ def draw_bounding_boxes(image, refs, jdx):
 
                         text_x = x1
                         text_y = max(0, y1 - 15)
-                            
+
                         text_bbox = draw.textbbox((0, 0), label_type, font=font)
                         text_width = text_bbox[2] - text_bbox[0]
                         text_height = text_bbox[3] - text_bbox[1]
-                        draw.rectangle([text_x, text_y, text_x + text_width, text_y + text_height], 
+                        draw.rectangle([text_x, text_y, text_x + text_width, text_y + text_height],
                                     fill=(255, 255, 255, 30))
-                        
+
                         draw.text((text_x, text_y), label_type, font=font, fill=color)
                     except:
                         pass
@@ -215,8 +216,8 @@ def draw_bounding_boxes(image, refs, jdx):
     return img_draw
 
 
-def process_image_with_refs(image, ref_texts, jdx):
-    result_image = draw_bounding_boxes(image, ref_texts, jdx)
+def process_image_with_refs(image, ref_texts, jdx, output_path):
+    result_image = draw_bounding_boxes(image, ref_texts, jdx, output_path)
     return result_image
 
 
@@ -231,100 +232,125 @@ def process_single_image(image):
 
 
 if __name__ == "__main__":
-
-    os.makedirs(OUTPUT_PATH, exist_ok=True)
-    os.makedirs(f'{OUTPUT_PATH}/images', exist_ok=True)
-    
-    print(f'{Colors.RED}PDF loading .....{Colors.RESET}')
-
-
-    images = pdf_to_images_high_quality(INPUT_PATH)
-
-
-    prompt = PROMPT
-
-    # batch_inputs = []
-
-    with ThreadPoolExecutor(max_workers=NUM_WORKERS) as executor:  
-        batch_inputs = list(tqdm(
-            executor.map(process_single_image, images),
-            total=len(images),
-            desc="Pre-processed images"
-        ))
-
-
-    # for image in tqdm(images):
-
-    #     prompt_in = prompt
-    #     cache_list = [
-    #         {
-    #             "prompt": prompt_in,
-    #             "multi_modal_data": {"image": DeepseekOCRProcessor().tokenize_with_images(images = [image], bos=True, eos=True, cropping=CROP_MODE)},
-    #         }
-    #     ]
-    #     batch_inputs.extend(cache_list)
-
-
-    outputs_list = llm.generate(
-        batch_inputs,
-        sampling_params=sampling_params
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '--input_path',
+        type=str,
+        required=True,
+        help='Path to input PDF file or a directory containing PDF files.',
     )
+    parser.add_argument(
+        '--output_path',
+        type=str,
+        required=True,
+        help='Path to output directory for processed images and results.',
+    )
+    args = parser.parse_args()
+    input_path = args.input_path
+
+    input_paths: list[str] = []
+    if os.path.isfile(input_path) and input_path.lower().endswith('.pdf'):
+        input_paths = [input_path]
+    elif os.path.isdir(input_path):
+        for f in os.listdir(input_path):
+            f_path = os.path.join(input_path, f)
+            if os.path.isfile(f_path) and f.lower().endswith('.pdf'):
+                input_paths.append(f_path)
+    else:
+        raise ValueError("Input path must be a PDF file or a directory containing PDF files.")
+
+    for input_path in input_paths:
+        file_basename = os.path.splitext(os.path.basename(input_path))[0]
+        output_path = os.path.join(args.output_path, file_basename)
+        os.makedirs(output_path, exist_ok=True)
+        os.makedirs(f'{output_path}/images', exist_ok=True)
+
+        print(f'{Colors.RED}PDF loading .....{Colors.RESET}')
 
 
-    output_path = OUTPUT_PATH
-
-    os.makedirs(output_path, exist_ok=True)
+        images = pdf_to_images_high_quality(input_path)
 
 
-    mmd_det_path = output_path + '/' + INPUT_PATH.split('/')[-1].replace('.pdf', '_det.mmd')
-    mmd_path = output_path + '/' + INPUT_PATH.split('/')[-1].replace('pdf', 'mmd')
-    pdf_out_path = output_path + '/' + INPUT_PATH.split('/')[-1].replace('.pdf', '_layouts.pdf')
-    contents_det = ''
-    contents = ''
-    draw_images = []
-    jdx = 0
-    for output, img in zip(outputs_list, images):
-        content = output.outputs[0].text
+        prompt = PROMPT
 
-        if '<｜end▁of▁sentence｜>' in content: # repeat no eos
-            content = content.replace('<｜end▁of▁sentence｜>', '')
-        else:
-            if SKIP_REPEAT:
-                continue
+        # batch_inputs = []
 
-        
-        page_num = f'\n<--- Page Split --->'
-
-        contents_det += content + f'\n{page_num}\n'
-
-        image_draw = img.copy()
-
-        matches_ref, matches_images, mathes_other = re_match(content)
-        # print(matches_ref)
-        result_image = process_image_with_refs(image_draw, matches_ref, jdx)
+        with ThreadPoolExecutor(max_workers=NUM_WORKERS) as executor:
+            batch_inputs = list(tqdm(
+                executor.map(process_single_image, images),
+                total=len(images),
+                desc="Pre-processed images"
+            ))
 
 
-        draw_images.append(result_image)
+        # for image in tqdm(images):
+
+        #     prompt_in = prompt
+        #     cache_list = [
+        #         {
+        #             "prompt": prompt_in,
+        #             "multi_modal_data": {"image": DeepseekOCRProcessor().tokenize_with_images(images = [image], bos=True, eos=True, cropping=CROP_MODE)},
+        #         }
+        #     ]
+        #     batch_inputs.extend(cache_list)
 
 
-        for idx, a_match_image in enumerate(matches_images):
-            content = content.replace(a_match_image, f'![](images/' + str(jdx) + '_' + str(idx) + '.jpg)\n')
+        outputs_list = llm.generate(
+            batch_inputs,
+            sampling_params=sampling_params
+        )
 
-        for idx, a_match_other in enumerate(mathes_other):
-            content = content.replace(a_match_other, '').replace('\\coloneqq', ':=').replace('\\eqqcolon', '=:').replace('\n\n\n\n', '\n\n').replace('\n\n\n', '\n\n')
+        mmd_det_path = os.path.join(output_path, f'{os.path.splitext(file_basename)[0]}_det.mmd')
+        mmd_path = os.path.join(output_path, f'{os.path.splitext(file_basename)[0]}.mmd')
+        print('mmd_det_path:', mmd_det_path)
+        pdf_out_path = os.path.join(output_path, f'{os.path.splitext(file_basename)[0]}_layouts.pdf')
+        contents_det = ''
+        contents = ''
+        draw_images = []
+        jdx = 0
+        for output, img in zip(outputs_list, images):
+            content = output.outputs[0].text
+
+            if '<｜end▁of▁sentence｜>' in content: # repeat no eos
+                content = content.replace('<｜end▁of▁sentence｜>', '')
+            else:
+                if SKIP_REPEAT:
+                    continue
 
 
-        contents += content + f'\n{page_num}\n'
+            page_num = f'\n<--- Page Split --->'
+
+            contents_det += content + f'\n{page_num}\n'
+
+            image_draw = img.copy()
+
+            matches_ref, matches_images, mathes_other = re_match(content)
+            # print(matches_ref)
+            result_image = process_image_with_refs(image_draw, matches_ref, jdx, output_path)
 
 
-        jdx += 1
-
-    with open(mmd_det_path, 'w', encoding='utf-8') as afile:
-        afile.write(contents_det)
-
-    with open(mmd_path, 'w', encoding='utf-8') as afile:
-        afile.write(contents)
+            draw_images.append(result_image)
 
 
-    pil_to_pdf_img2pdf(draw_images, pdf_out_path)
+            for idx, a_match_image in enumerate(matches_images):
+                content = content.replace(a_match_image, f'![](images/' + str(jdx) + '_' + str(idx) + '.jpg)\n')
+
+            for idx, a_match_other in enumerate(mathes_other):
+                content = content.replace(a_match_other, '').replace('\\coloneqq', ':=').replace('\\eqqcolon', '=:').replace('\n\n\n\n', '\n\n').replace('\n\n\n', '\n\n')
+
+
+            # contents += content + f'\n{page_num}\n'
+            contents += content
+
+
+            jdx += 1
+
+        with open(mmd_det_path, 'w', encoding='utf-8') as afile:
+            afile.write(contents_det)
+
+        with open(mmd_path, 'w', encoding='utf-8') as afile:
+            afile.write(contents)
+
+
+        pil_to_pdf_img2pdf(draw_images, pdf_out_path)
 
